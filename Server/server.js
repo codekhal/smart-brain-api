afb5c558b72306b598d620a1422ab573f4ab991b
khushal <vyas.khushal1999@gmail.com>
2019-04-26 00:31:30 +0530

    deleted node_modules folder

---
+++ Server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Khushal',
+            email: 'khushal@gmail.com',
+            password: 'khush',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Khushi',
+            email: 'khushi@gmail.com',
+            password: 'khushi',
+            entries: 0,
+            joined: new Date()
+        },
+
+
+    ]
+}
+
+app.get('/',(req,res) => {
+    res.send("its working!");
+})
+
+app.post('/signin',(req,res) => {
+    if(req.body.email === database.users[0].email && 
+        req.body.name === database.users[0].name) {
+            res.json('success');
+        }
+    else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req,res) => {
+    database.users.push({
+        id: '125',
+        name: 'vandu',
+        email: 'vandu@gmail.com',
+        password: 'vandu',
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length -1]);
+})
+
+
+app.listen(3000, () => {
+    console.log("app is running!");
+})
